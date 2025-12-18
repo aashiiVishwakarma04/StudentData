b29ce7855f3cbe0e1879cf4b27fd01bbb63fb263
DELL <DELL@DESKTOP-V2AQ26Q>
2025-12-18 22:49:23 +0530

    Simple Student Form

---
+++ src/com/student/Student.java
@@ -0,0 +1,12 @@
+package com.student;
+ public class Student{
+	 public static void main(String args[]) {
+		 String name;
+		 int age;
+		 String gender;
+		 
+		
+	 }
+ }
+
+	
---
+++ src/com/student/UserInput.java
@@ -0,0 +1,58 @@
+package com.student;
+import  java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Please Enter Your Name");
+     String name = sc.nextLine();
+     
+     System.out.println("Pleasse Enter Your DOB");
+     String DOB = sc.nextLine();
+     
+     System.out.println("Please Enter Your age");
+     int age = sc.nextInt();
+     sc.nextLine();
+     
+     System.out.println("Please Enter Your Blood Group");
+     String bloodgroup = sc.nextLine();
+     
+     System.out.println("Please Enter Your Nationality");
+     String nationality = sc.nextLine();
+     
+     System.out.println("Please Enter Your Marital Status");
+      String status = sc.nextLine();
+      
+      System.out.println("Please Enter Your Gender");
+       String gender = sc.nextLine();
+       
+       System.out.println("Please Enter Your Adhar Number");
+       String adharNumber = sc.nextLine();
+       
+       System.out.println("Please Enter Your Email Id");
+       String email = sc.nextLine();
+       
+       System.out.println("Please Enter Your Number");
+        String number =  sc.nextLine();
+        
+        System.out.println("Please Enter Your Address");
+        String address = sc.nextLine();
+        
+        System.out.println("Please Enter Your College Name");
+        String collegename = sc.nextLine();
+        
+        System.out.println("Enter Your CollegeID");
+        String ID = sc.nextLine();
+        
+        System.out.println("Please Enter Your Course Name");
+        String course = sc.nextLine();
+        
+        System.out.println("Please Enter Your Branch");
+        String branch = sc.nextLine();
+        
+ 
+	}
+
+}
